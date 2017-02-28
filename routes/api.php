ce66f87dc33b0438c28f263e792d5bf1634720aa
FlyBirdHeight <531020509@qq.com>
2017-02-28 20:10:30 +0800

    Authorizationh和Bearer 的使用，来对用户的api进行验证

--- routes/api.php
+++ routes/api.php
@@ -27,9 +27,7 @@ Route::middleware('api')->get('/topics',function (Request $request){
 Route::middleware('auth:api')->post('/question/follower', function (Request $request) {
     //这玩意能够防止有心的用户恶意的多次请求，通过登陆进来的用的api来获取用户的资料！
     $user = \Illuminate\Support\Facades\Auth::guard('api')->user();
-    $follow = !! \App\Follow::where('question_id',$request->get('question'))
-                            ->where('user_id',$user->id)
-                            ->count();
+    $follow = !! $user->followed($request->get('question'));
     if($follow){
         return response()->json(['followed' => true]);
     }
@@ -38,16 +36,16 @@ Route::middleware('auth:api')->post('/question/follower', function (Request $req
 
 Route::middleware('auth:api')->post('/question/follow', function (Request $request) {
     $user = \Illuminate\Support\Facades\Auth::guard('api')->user();
+    $question = \App\Question::find($request->get('question'));
     $follow = \App\Follow::where('question_id',$request->get('question'))
         ->where('user_id',$user->id)
         ->first();
     if($follow !== null){
         $follow->delete();
+        $question->decrement('follower_count');
         return response()->json(['followed' => false]);
     }
-    \App\Follow::create([
-        'user_id' => $user->id,
-        'question_id' => $request->get('question')
-    ]);
+    $user->followThis($request->get('question'));
+    $question->increment('follower_count');
     return response()->json(['followed' => true]);
 });
